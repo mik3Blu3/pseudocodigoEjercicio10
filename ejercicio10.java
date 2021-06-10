18334b1497f50ce1ab2f42f456a5237ad491ecb8
mik3Blu3 <enanin12992@gmail.com>
2021-06-10 12:11:32 -0600

    Agregue el ejercicio10 en lenguaje Java

---
+++ ejercicio10.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class ejercicio10 
+{
+    static Scanner entrada = new Scanner(System.in);
+    public static void main(String args[])
+    {
+        int sumaNumeros = 0;
+        int sumaNumerosAux =0;
+        int numAuxMod = 0;
+        int numMultiplos = 0;
+        System.out.println("Se leeran los numeros hasta que la suma de los mismos llegue a 1000");
+        while(sumaNumeros <= 1000)
+        {
+            System.out.println("Ingrese el numero:");
+            int num = entrada.nextInt();
+            sumaNumeros = (sumaNumeros+num);
+            numAuxMod = (num % 6);
+            if(numAuxMod == 0)
+            {
+                numMultiplos = (numMultiplos+1);
+            }
+            if((num >= 1) && (num <= 10))
+            {
+                sumaNumerosAux = (sumaNumerosAux+1);
+            }
+        }
+        System.out.println("La cantidad de numeros multiplos de 6 encontrados es: "+numMultiplos);
+        System.out.println("La suma de los numeros encontrados entre (1,10) es: "+sumaNumerosAux);
+    }
+}
